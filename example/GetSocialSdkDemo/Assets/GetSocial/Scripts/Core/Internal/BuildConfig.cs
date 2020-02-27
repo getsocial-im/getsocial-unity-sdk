3bfd1e3a8bb6c2947823a9f8635314f159d3fb14
GetSocial <support@getsocial.im>
2020-02-27 11:21:28 +0100

    Release demo app for GetSocial SDK version 6.32.2

--- example/GetSocialSdkDemo/Assets/GetSocial/Scripts/Core/Internal/BuildConfig.cs
+++ example/GetSocialSdkDemo/Assets/GetSocial/Scripts/Core/Internal/BuildConfig.cs
@@ -6,7 +6,7 @@ namespace GetSocialSdk.Core
         /// <summary>
         /// Version of GetSocial Unity SDK
         /// </summary>
-        public const string UnitySdkVersion = "6.32.1";
+        public const string UnitySdkVersion = "6.32.2";
 
         /// <summary>
         /// Publishing target for the unitypackage.
