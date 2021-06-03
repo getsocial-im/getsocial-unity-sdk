4b6feefd9836161f3db391510d0036494fc47940
GetSocial <support@getsocial.im>
2021-06-03 14:09:44 +0200

    Release demo app for GetSocial SDK version 7.4.5

--- example/GetSocialSdkDemo/Assets/GetSocial/Editor/GetSocialGradlePostProcess.cs
+++ example/GetSocialSdkDemo/Assets/GetSocial/Editor/GetSocialGradlePostProcess.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using GetSocialSdk.Core;
 using UnityEditor;
@@ -17,21 +18,62 @@ namespace GetSocialSdk.Editor
         public void OnPostGenerateGradleAndroidProject(string path)
         {
             Debug.Log("GetSocialGradlePostProcess.OnPostGenerateGradleAndroidProject at path " + path);
+            string jsonSourceFilePath = Path.Combine(Application.streamingAssetsPath, "getsocial.json");
+            string jsonTargetFilePath;
+            string mainGradleFilePath;
+            string gradleFilePath;
 #if UNITY_2019_3_OR_NEWER
-            File.AppendAllText(path + "/../build.gradle", mainGradleText());
-            File.AppendAllText(path + "/../launcher/build.gradle", gradleText());
-            FileUtil.CopyFileOrDirectory(Path.Combine(Application.streamingAssetsPath, "getsocial.json"), Path.Combine(path, "../launcher/getsocial.json"));
+            mainGradleFilePath = path + "/../build.gradle";
+            gradleFilePath = path + "/../launcher/build.gradle";
+            jsonTargetFilePath = Path.Combine(path, "../launcher/getsocial.json");
 #else
-            File.AppendAllText(path + "/build.gradle", mainGradleText());
-            File.AppendAllText(path + "/build.gradle", gradleText());
-            FileUtil.CopyFileOrDirectory(Path.Combine(Application.streamingAssetsPath, "getsocial.json"), Path.Combine(path, "getsocial.json"));
+            mainGradleFilePath = path + "/build.gradle";
+            gradleFilePath = path + "/build.gradle";
+            jsonTargetFilePath = Path.Combine(path, "getsocial.json");
 #endif
+            if (File.Exists(mainGradleFilePath))
+            {
+                var modifiedGradleProperties = new List<string>();
+                var lines = File.ReadAllLines(mainGradleFilePath);
+                bool mavenRepoAdded = false;
+                bool dependencyAdded = false;
+
+                foreach (string line in lines)
+                {
+
+                    modifiedGradleProperties.Add(line);
+                    if (line.Contains(gradle_repositories) && !mavenRepoAdded)
+                    {
+                        modifiedGradleProperties.Add("\t\tmaven {");
+                        modifiedGradleProperties.Add("\t\t\turl 'https://plugins.gradle.org/m2/'");
+                        modifiedGradleProperties.Add("\t\t}");
+                        mavenRepoAdded = true;
+                    }
+                    if (line.Contains(gradle_dependencies) && !dependencyAdded)
+                    {
+                        modifiedGradleProperties.Add("\t\tclasspath 'im.getsocial:plugin-v7:[1,2)'");
+                        dependencyAdded = true;
+                        
+                    }
+                }
+                File.WriteAllText(mainGradleFilePath, string.Join("\n", modifiedGradleProperties.ToArray()) + "\n");
+            }
+            else {
+                File.AppendAllText(mainGradleFilePath, mainGradleText());
+            }
+            File.AppendAllText(gradleFilePath, gradleText());
+            FileUtil.CopyFileOrDirectory(jsonSourceFilePath, jsonTargetFilePath);
+
             if (PlayerSettings.applicationIdentifier == "com.Company.ProductName")
             {
                 Debug.LogError(
                     "GetSocial: Please change the default Unity Bundle Identifier (com.Company.ProductName) to your package.");
             }
         }
+
+        private static string gradle_repositories = "repositories";
+        private static string gradle_dependencies = "dependencies";
+
         private static string mainGradleText() {
             return @"
 allprojects {
--- example/GetSocialSdkDemo/Assets/GetSocial/Scripts/Core/Internal/BuildConfig.cs
+++ example/GetSocialSdkDemo/Assets/GetSocial/Scripts/Core/Internal/BuildConfig.cs
@@ -6,7 +6,7 @@ namespace GetSocialSdk.Core
         /// <summary>
         /// Version of GetSocial Unity SDK
         /// </summary>
-        public const string UnitySdkVersion = "7.4.4";
+        public const string UnitySdkVersion = "7.4.5";
 
         /// <summary>
         /// Publishing target for the unitypackage.
